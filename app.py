14f4d26a64fe92170511abcefab55436ef437f18
Rohit Deshpande <31229564+123Rohit123@users.noreply.github.com>
2025-08-19 17:58:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from typing import List, Dict, Set
 import urllib.parse
 
 st.set_page_config(page_title="Recipe Bot", page_icon="🥘",
-                   layout="short", initial_sidebar_state="expanded")
+                   layout="wide", initial_sidebar_state="expanded")
 st.title("🥘 Recipe Bot")
 st.caption("Pick what you have. I’ll suggest recipes with steps and a related YouTube video.")
 
